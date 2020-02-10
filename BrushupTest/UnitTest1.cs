4972dc379b5c523f5f4a9497e8177425df13b257
Peter Levinsky <pele@easj.dk>
2020-02-10 09:50:49 +0100

    Nu med UnitTest

---
+++ BrushupTest/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BrushuoLib.model;
+
+namespace BrushupTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Person p = new Person("peter", 61, true);
+            p.Age = -1;
+            p.Name = "peter";
+
+            Assert.AreEqual("peter", p.Name);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Person p = new Person("peter", 61, true);
+            Assert.ThrowsException<ArgumentException>(() => p.Name = "pete");
+            Assert.ThrowsException<ArgumentException>(()=> p.Name = null);
+        }
+
+
+    }
+}
---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using BrushuoLib.model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // arrange
+            Person p = new Person("SomeName",23,true);
+            String expectedName = "Peter";
+
+            // act
+            p.Name = "Peter";
+            string actualName = p.Name;
+
+            //assert
+            Assert.AreEqual(expectedName,actualName);
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            // arrange
+            Person p = new Person("SomeName", 23, true);
+
+            // Act + assert
+            Assert.ThrowsException<Exception>( ()=> p.Name = "pete" );
+
+
+        }
+
+
+    }
+}
